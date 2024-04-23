27b4acc4505db1a5b2bef476b26c85bb9acaee24
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-04-23 23:46:58 +0200

    refactor(contracts): premint tokens

--- contracts/src/MyToken.sol
+++ contracts/src/MyToken.sol
@@ -7,7 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {
+        _mint(msg.sender, 1234 * 10 ** decimals());
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
