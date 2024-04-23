db26b984ae2cdb3ccd08e16491b8c4c629780625
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-04-24 00:34:53 +0200

    fix(contracts): deployment params

--- contracts/ignition/params.ts
+++ contracts/ignition/params.ts
@@ -1,11 +1,11 @@
 export const base = {
     chain: 10004n,
-    owner: '0xa74b434bBD3d9bfdF522c05C4EEEf4056d945bfe',
+    owner: '0xC37713ef41Aff1A7ac1c3D02f6f0B3a57F8A3091',
     relayer: '0x93BAD53DDfB6132b0aC8E37f6029163E63372cEE',
 };
 
 export const optimism = {
     chain: 10005n,
-    owner: '0xa74b434bBD3d9bfdF522c05C4EEEf4056d945bfe',
-    relayer: '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+    owner: '0xC37713ef41Aff1A7ac1c3D02f6f0B3a57F8A3091',
+    relayer: '0x93BAD53DDfB6132b0aC8E37f6029163E63372cEE',
 };
